f0d9b982b449d2f460c3a953dacd218a88edc1de
Alexander Rodnin <Alexander.Rodnin@gmail.com>
2021-04-25 19:24:59 +0300

    Initial commit

---
+++ src/test/java/ru/otus/microservicearchitecture/lesson5/lesson5/Lesson5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.microservicearchitecture.lesson5.lesson5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
